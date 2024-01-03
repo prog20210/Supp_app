e6b22fe553fdc755c3ace2cfe7f77e8751b72f49
avsemykin2016 <avsemykin2016@yandex.ru>
2024-01-03 18:07:44 +0300

    удален модуль ef

---
+++ AppContext.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class AppContext:DbContext
+{
+	public Class1()
+	{
+	}
+}
+
---
+++ WebApplication1/context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Npgsql;
+
+namespace WebApplication1
+{
+    public class context : DbContext
+    {
+
+        private const string comstr = @"User ID = postgres; Password=12345;Host=localhost;Port=5432;Database=sup;Pooling=true;Min Pool Size=0;Max Pool Size=100;Connection Lifetime = 0;";
+
+
+
+    }
+}
+    
