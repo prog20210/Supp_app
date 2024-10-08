1839c43bc01a487e6cb4d3a542f9143137779467
andrey <andrey@AVS>
2024-10-08 20:39:26 +0400

    Исправлена проблема с Reader. Подготовка к заполнению формы данными из базы

--- WebApplication1/request.aspx.cs
+++ WebApplication1/request.aspx.cs
@@ -6,7 +6,9 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using JSPool;
 using Microsoft.AspNetCore.Mvc.RazorPages;
- 
+using Npgsql;
+using System.Configuration;
+
 
 
 namespace WebApplication1
@@ -20,13 +22,13 @@ namespace WebApplication1
             string eml = "avsemykin2016@yandex.ru";
             Zap(eml);
 
-           
-         
 
 
 
 
-                 
+
+
+
 
 
 
@@ -34,18 +36,99 @@ namespace WebApplication1
         }
 
 
-        public  string Zap(string mail)
+        public string Zap(string mail)
         {
-            string a = mail;
 
-            //Заполнение формы =заполнение контекста HTTP 
-            HttpContext.Current.Items["email"] = "avsemykin2016@yandex.ru";
-            HttpContext.Current.Items["trademark"] = "kljlkjkljkljlkjkljkljklj";
+
+
+            ConnectionStringSettings constr;
+            constr = ConfigurationManager.ConnectionStrings["con"];
+
+            NpgsqlConnection con = new NpgsqlConnection();
+            NpgsqlCommand com = con.CreateCommand();
+
+            constr = ConfigurationManager.ConnectionStrings["con"];
+
+
+
+
+
+            ///con.ConnectionString = constr[0]
+
+
+            con.ConnectionString = constr.ToString();
+
+            //"Server = 127.0.0.1; Port = 5433; Database = sup; User Id = postgres; Password =12345";
+
+            try
+            {
+               
+                con.Open();
+               
+               
+
+
+                int num;
+                int numbn;
+                int i = 0;
+                //com.CommandText = "select max(id) from req";
+
+
+                com.CommandText = "select * from users";
+                NpgsqlDataReader rd = com.ExecuteReader();
+
+
+
+                if (rd.HasRows) {
+
+                    while (rd.Read())
+                    {
+                        string gg = rd.GetString(i);
+                        i = i + 1;
+                    }
+
+
+                }
+                //Заполнение формы =заполнение контекста HTTP 
+                HttpContext.Current.Items["email"] = "avsemykin2016@yandex.ru";
+                HttpContext.Current.Items["trademark"] = "kljlkjkljkljlkjkljkljklj";
+
+
+
+                //Заполнение формы =заполнение контекста HTTP 
+                HttpContext.Current.Items["email"] =rd[0].ToString();
+                HttpContext.Current.Items["trademark"] = rd[1].ToString();
+                return mail;
+
+
+            }
+
+
+
+
+
+            catch { }
+
+
+
+
+
+
+
+
+
+            //"Server = 127.0.0.1; Port = 5433; Database = sup; User Id = postgres; Password =12345";
+
+
+           
             return mail;
 
-        }
-        
-        
 
+
+
+
+        }
+  
     }
+
 }
